48940e2eb18d4f8c01692528e5ee4f9514a5d2c8
anubhavcu <anubhavcs.py@gmail.com>
2022-04-24 06:55:15 +0530

    added leetcode ques for twoSum

---
+++ leetcode/1twoSum.js
@@ -0,0 +1,19 @@
+// link : https://leetcode.com/problems/two-sum/
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+var twoSum = function (nums, target) {
+  let isSeen = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    if (isSeen[target - n] !== undefined) {
+      return [isSeen[target - n], i];
+    }
+    isSeen[n] = i;
+  }
+  return [];
+};
