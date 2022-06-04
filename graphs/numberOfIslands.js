041a8c10f64d4cb7a6d9c2a46a7b6762bf917ff2
anubhavcu <anubhavcs.py@gmail.com>
2022-06-05 02:39:01 +0530

    number of islands -lc | graph

---
+++ graphs/numberOfIslands.js
@@ -0,0 +1,57 @@
+//https://leetcode.com/problems/number-of-islands/
+// Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+// An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+// Example 1:
+
+// Input: grid = [
+//   ["1","1","1","1","0"],
+//   ["1","1","0","1","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","0","0","0"]
+// ]
+// Output: 1
+// Example 2:
+
+// Input: grid = [
+//   ["1","1","0","0","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","1","0","0"],
+//   ["0","0","0","1","1"]
+// ]
+// Output: 3
+
+var numIslands = function (grid) {
+  if (grid === null || grid.length === 0) {
+    return 0;
+  }
+
+  let ans = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === '1') {
+        ans += dfs(grid, i, j);
+      }
+    }
+  }
+  return ans;
+};
+function dfs(grid, i, j) {
+  if (
+    i < 0 ||
+    i >= grid.length ||
+    j < 0 ||
+    j >= grid[i].length ||
+    grid[i][j] === '0'
+  ) {
+    return 0;
+  }
+
+  grid[i][j] = '0';
+  dfs(grid, i + 1, j);
+  dfs(grid, i - 1, j);
+  dfs(grid, i, j + 1);
+  dfs(grid, i, j - 1);
+  return 1;
+}
